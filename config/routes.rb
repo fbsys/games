2887628d64c4c2d377c2476de927aaedf593e04d
hamachiyo <autumn.c.6105503@gmail.com>
2014-09-07 13:58:24 +0900

    login added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   resources :work_tasks
 
   resources :work_processes
@@ -14,6 +16,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'avatars#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
